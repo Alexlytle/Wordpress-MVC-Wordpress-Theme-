153a8ab5973269fcc71bf4ee2928793b69722480
Alexlytle <44213950+Alexlytle@users.noreply.github.com>
2022-01-10 10:04:00 -0800

    test
    tester

--- functions.php
+++ functions.php
@@ -199,14 +199,5 @@ Routes::map('example/:example', function($params){
 
 
 
-require 'plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/Alexlytle/WebFieldTheme/',
-	__FILE__,
-	'https://webfielddesign.com/'
-);
 
-//Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('main');
-$myUpdateChecker->setAuthentication('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');
 
